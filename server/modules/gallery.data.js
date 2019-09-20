38c2fa2bf7d61f27cd44a545bde31e0c6fe0e6b5
Jake Ford <jakemford17@gmail.com>
2019-09-20 16:06:59 -0500

    Added photos to images folder/updated gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,16 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/bb2011.jpg', description: 'Photo from the 2011 Stanley Cup Champion Boston Bruins', likes: 0 },
+    { id: 2, path: 'images/bc2008.jpg', description: 'Photo from the 2008 NBA Champion Boston Celtics', likes: 0 },
+    { id: 3, path: 'images/brs2004.jpg', description: 'Photo from the 2004 World Series Champion Boston Red Sox', likes: 0 },
+    { id: 4, path: 'images/brs2007.jpg', description: 'Photo from the 2007 World Series Champion Boston Red Sox', likes: 0 },
+    { id: 5, path: 'images/brs2013.jpg', description: 'Photo from the 2013 World Series Champion Boston Red Sox', likes: 0 },
+    { id: 6, path: 'images/brs2018.jpg', description: 'Photo from the 2018 World Series Champion Boston Red Sox', likes: 0 },
+    { id: 7, path: 'images/nep2001.jpg', description: 'Photo from the Super Bowl XXXVI Champion New England Patriots', likes: 0 },
+    { id: 8, path: 'images/nep2003.jpg', description: 'Photo from the Super Bowl XXXVIII Champion New England Patriots', likes: 0 },
+    { id: 9, path: 'images/nep2004.jpg', description: 'Photo from the Super Bowl XXXIX Champion New England Patriots', likes: 0 },
+    { id: 10, path: 'images/nep2014.jpg', description: 'Photo from the Super Bowl XLIX Champion New England Patriots', likes: 0 },
+    { id: 11, path: 'images/nep2016.jpg', description: 'Photo from the Super Bowl LI Champion New England Patriots', likes: 0 },
+    { id: 12, path: 'images/nep2018.jpg', description: 'Photo from the Super Bowl LIII Champion New England Patriots', likes: 0 }
 ];
 
-module.exports = galleryItems;
+module.exports = galleryItems; 
